53837771738cea4e24f96d829bc68a082410c03b
ferchichihadil <ferchichihadil1@gmail.com>
2024-04-18 12:09:53 +0100

    time sheet app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ServiceComponent } from './service/service.component';
+import { ProjetComponent } from './projet/projet.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home',component: AccueilComponent},
+  { path:'services',component: ServiceComponent},
+  { path:'projects',component: ProjetComponent},
+  { path:'aboutus',component: AboutComponent},
+  { path:'signup',component: RegisterUserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,50 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ServiceComponent } from './service/service.component';
+import { ProjetComponent } from './projet/projet.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RegisterUserComponent,
+    AccueilComponent,
+    ServiceComponent,
+    ProjetComponent,
+    AboutComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
